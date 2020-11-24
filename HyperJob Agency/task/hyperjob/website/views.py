bfda2bbcb14cb061aeb92aeeeb927c33d7312c3e
msmilkshake <prnfilipe@gmail.com>
2020-11-24 00:36:06 +0000

    stage2 complete

--- HyperJob Agency/task/hyperjob/website/views.py
+++ HyperJob Agency/task/hyperjob/website/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 
+menu = {
+    'login': 'Login',
+    'signup': 'Signup',
+    'vacancies': 'Available Vacancies',
+    'resumes': 'Available Resumes',
+    'home': 'Your Profile'
+}
 
 # Create your views here.
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'website/index.html')
+        return render(request, 'website/index.html', context={'menu': menu})
